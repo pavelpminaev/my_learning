b9a432550b92b1066388aa41cc790ed900ca4761
pavelpminaev <pavelpminaev@gmail.com>
2023-01-21 18:07:33 +0600

    create example.py

---
+++ example.py
@@ -0,0 +1,16 @@
+def reverse(string):
+    """Reverse string
+
+    >>> reverse('')
+    ''
+
+    >>> reverse('Hexlet')
+    'telxeH'
+    """
+
+    return string[::-1]
+
+# Нужно для запуска тестов
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
