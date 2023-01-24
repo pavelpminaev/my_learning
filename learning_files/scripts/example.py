a7d3fcf0db32ebfe8e5e67008144f6b26399bb4c
pavelpminaev <pavelpminaev@gmail.com>
2023-01-25 01:56:25 +0600

    init commit

---
+++ learning_files/scripts/example.py
@@ -0,0 +1,8 @@
+# filter out all odd numbers
+
+
+li = [5, 7, 22, 97, 54, 62, 77, 23, 73, 61]
+final_list = list(filter(lambda x: (x % 2 != 0), li))
+print(final_list)
+
+# filter out all odd numbers
