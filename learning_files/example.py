1e99ccde2d8bb2d871ad36a52d1eb53c92574d11
pavelpminaev <pavelpminaev@gmail.com>
2023-01-25 21:01:51 +0600

    refactoring

--- learning_files/example.py
+++ learning_files/example.py
@@ -1,4 +1,4 @@
-
+"""It's example doctest using"""
 
 def reverse(string):
     """Reverse string.
--- learning_files/scripts/example.py
+++ learning_files/scripts/example.py
@@ -1,8 +1,7 @@
-# filter out all odd numbers
+"""It's example doctest using."""
 
-
-li = [5, 7, 22, 97, 54, 62, 77, 23, 73, 61]
-final_list = list(filter(lambda x: (x % 2 != 0), li))
+li = [5, 7, 22, 97, 54, 62, 77, 23, 73, 61]  # filter out all odd numbers
+final_list = list(filter(lambda ite: (ite % 2 != 0), li))
 print(final_list)
 
 # filter out all odd numbers
