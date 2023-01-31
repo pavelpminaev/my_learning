c9fa620e6243abdb3f214436c2018d557511a061
pavelpminaev <pavelpminaev@gmail.com>
2023-02-01 01:05:48 +0600

    lerning in-out with files

--- learning_files/input_output_in_python/input_output_functions.py
+++ learning_files/input_output_in_python/input_output_functions.py
@@ -1,7 +1,29 @@
 f = open('input_file.txt', 'w')
-f.write('Hello\nWorld')
+f.writelines(['Hello\nWorld\n', 'cat\n', 'dog\n'])
 f.close()
+f = open('input_file.txt', 'r')
+print(f.readline())
+print(f.readline())
+print(f.read())
+print('---------------------')
 
+f.close()
+f = open('input_file.txt', 'r')
+for l in f:
+    print(l)
+print('---------------------')
+f.close()
+f = open('input_file.txt', 'r')
+for l in f:
+    print(l)
+    break
+for l in f:
+    print(l)
+    break
+print('---------------------')
+
+
+f = open('input_file.txt', 'r')
 
 """with open('input.txt', 'r') as input_file:
     with open('output.txt', 'w') as output_file:
