5b358494606d5fe76726ed97889b94d4b4dc6521
pavelpminaev <pavelpminaev@gmail.com>
2023-02-01 01:23:29 +0600

    lerning in-out with files

--- learning_files/input_output_in_python/input_output_functions.py
+++ learning_files/input_output_in_python/input_output_functions.py
@@ -1,29 +1,40 @@
+print('----------writing and writelines-----------')
 f = open('input_file.txt', 'w')
-f.writelines(['Hello\nWorld\n', 'cat\n', 'dog\n'])
+print(f.write('Hello\nWorld\n'))   # 12
+print(f.writelines(['cat\n', 'dog\n']))   # None
 f.close()
-f = open('input_file.txt', 'r')
-print(f.readline())
-print(f.readline())
-print(f.read())
-print('---------------------')
 
+print('----------read line-----------')
+f = open('input_file.txt', 'r')
+print(f.readline())     # Hello
+print(f.readline())     # World
+print(f.read())         # Hello\n World\n cat\n dog\n
+f.close()
+
+print("---printing lines with 'for'---")
 f.close()
 f = open('input_file.txt', 'r')
 for l in f:
     print(l)
-print('---------------------')
+f.close()
+
+print("---printing lines with 'for' and 'break'---")
 f.close()
 f = open('input_file.txt', 'r')
 for l in f:
     print(l)
     break
+
 for l in f:
     print(l)
     break
+f.close()
+
 print('---------------------')
-
-
 f = open('input_file.txt', 'r')
+
+
+
 
 """with open('input.txt', 'r') as input_file:
     with open('output.txt', 'w') as output_file:
