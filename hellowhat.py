9e13119d47e9e466d0bdf3dbab6b981cbd04ba3c
pavelpminaev <pavelpminaev@gmail.com>
2023-12-16 00:58:34 +0600

    learning use if(func) for password control

---
+++ hellowhat.py
@@ -0,0 +1,24 @@
+"""print('Hello. What is your name?')
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is: ')
+print(len(myName))
+print('What is your age?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year')
+
+if myName == 'Pavel':
+	print('Hello Pavel input your password')
+	password = input()
+	if password == 'sfish':
+		print('Access granted')
+	else:
+		print('Wrong password')
+else:
+	print('User not registred')
+"""
+total = 0
+for num in range(1001):
+	total = total + num
+print(total)
+
